beec1cadaf9fd0a940766dadaa744ef3b61946a2
우수 <woosu91@gmail.com>
2026-08-11 13:53:40 +0900

    Revert "fix: Render Docker 시작 명령 수정"

--- systems/backend/render_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-python -m ontology_dashboard.bootstrap
-exec python -m uvicorn ontology_dashboard.app:app \
-  --host 0.0.0.0 \
-  --port "${PORT:-10000}" \
-  --no-server-header
